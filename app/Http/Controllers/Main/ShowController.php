6788dad97951b6629c06c34c3ff17b1083e6c388
TomatoMan <fillippok87@gmail.com>
2024-11-30 00:18:34 +0700

    related post and carbon

--- app/Http/Controllers/Main/ShowController.php
+++ app/Http/Controllers/Main/ShowController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
 
 class ShowController extends Controller
 {
@@ -11,6 +13,14 @@ class ShowController extends Controller
     {
         $post = Post::findOrFail($id);
 
-        return view('main.show', compact('post'));
+        $date = Carbon::parse($post->created_at);
+
+        $relatedPosts = Post::where('category_id', $post->category_id)
+            ->where('id', '!=', $id)
+            ->get();
+        $randomRelated = $relatedPosts->random(fn (Collection $items) => min(4, count($items)));
+
+
+        return view('main.show', compact('post' , 'date', 'randomRelated'));
     }
 }
--- resources/views/main/show.blade.php
+++ resources/views/main/show.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.main')
 
 @section('content')
-    <p> {{ $post->id }}: {{ $post->title }}</p>
+    <div class="">
+        <div class="post-title-wrap">
+            <p class="post-title-text">{{ $post->title }}</p>
+            <p class="post-title-create">{{ $date->translatedFormat('d F Y H:i') }}</p>
+            <p class="post-title-comment">Комментариев: {{ $post->comments->count() }}</p>
+        </div>
+        <div class="post-img-wrap">
+            <img src="" alt="img">
+        </div>
+        <div class="post-text-wrap">
+            <p class="post-text">{{ $post->content }}</p>
+        </div>
+
+        <div class="related-wrap">
+            <div class="related-title-wrap">
+                <h3 class="related-title">Похожие посты</h3>
+            </div>
+            @foreach($randomRelated as $relPost)
+                <div class="rel-card-wrap">
+                    <div class="rel-card">
+                        <div class="rel-img-wrap">
+                            <img src="" alt="img">
+                        </div>
+                        <div class="rel-title-wrap">
+                            <p class="rel-title">{{ $relPost->title }}</p>
+                        </div>
+                        <div class="rel-link-wrap">
+                            <a href="{{ route('main.show', $relPost->id) }}">Читать</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
 
 
 @endsection
